a5e7607507089d5816a87eb03faaa261a21c9830
ivan4000 <sxln1@mail.ru>
2018-11-02 18:09:18 +0300

    JL109PopupMenu

---
+++ src/com/Main.java
@@ -0,0 +1,39 @@
+package com;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+    static JFrame jFrame = getFrame();
+    static JPanel jPanel = new JPanel();
+
+    public static void main(String[] args) {
+        jFrame.add(jPanel);
+        JPopupMenu jPopupMenu=new JPopupMenu();
+
+        JMenuItem exit= new JMenuItem("Exit");
+        exit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        jPopupMenu.add(exit);
+        jPopupMenu.add(new JMenuItem("Two"));
+        jPanel.setComponentPopupMenu(jPopupMenu);
+
+    }
+    static JFrame getFrame() {
+        JFrame jFrame = new JFrame() {
+        };
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        jFrame.setBounds(dimension.width / 2 - 250, dimension.height / 2 - 150, 500, 500);
+        return jFrame;
+
+    }
+}
